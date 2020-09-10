c322b390cf5bc2fd58f1e76d3d0050cbf9e23da8
Clem Nkl <clem.nkl@gmail.com>
2020-09-10 15:29:47 +0200

    Changement page produit
    Contenants dans la page HTML avec remplissage grâce au JS

--- js/produit.js
+++ js/produit.js
@@ -24,104 +24,84 @@ function teddyGet() {
 };
 
 //Eléments pour construire le bloc présentation de la peluche
-let teddy = document.getElementById("teddy");
-let divImg = document.createElement("div");
-divImg.classList.add("col-6");
+let divImg = document.getElementById("divImg");
 let myImg = new Image();
 myImg.addEventListener('load', function () { });
 myImg.classList.add('teddy-img');
-let divDescription = document.createElement("div");
-divDescription.classList.add("col-6");
-let nameTeddy = document.createElement("h3");
-nameTeddy.classList.add('text-center');
-let teddyDescription = document.createElement("p");
-let teddyPrice = document.createElement("p");
-let divColor = document.createElement('div');
-divColor.classList.add('div-color');
-let labelColor = document.createElement("p");
-let choiceColor = document.createElement("select");
-let divQuantité = document.createElement('div');
-divQuantité.classList.add('div-quantite');
-let addCart = document.createElement("button");
-addCart.classList.add("btn", "btn-lg");
+let nameTeddy = document.getElementById("nameTeddy");
+let teddyDescription = document.getElementById("description");
+let teddyPrice = document.getElementById("price");
+let choiceColor = document.getElementById("choiceColor");
+let choiceAmount = document.getElementById("choiceAmount");
+let addCart = document.getElementById("addCart");
 
-// Création du bloc descriptif de la peluche
+// Remplissage du bloc descriptif de la peluche
 teddyGet()
     .then(function (response) {
-        document.title=response["name"];
+        document.title = response["name"];
         myImg.src = response["imageUrl"];
-        teddy.appendChild(divImg).appendChild(myImg);
-        teddy.appendChild(divDescription).appendChild(nameTeddy).innerHTML = response["name"];
-        teddy.appendChild(divDescription).appendChild(teddyDescription).innerHTML = response["description"];
-        teddy.appendChild(divDescription).appendChild(teddyPrice).innerHTML = "Prix : " + response["price"] / 100 + "€";
-        teddy.appendChild(divDescription).appendChild(divColor).appendChild(labelColor).innerHTML = "Veuillez choisir une couleur : ";
-
-    })
+        divImg.appendChild(myImg);
+        nameTeddy.innerHTML = response["name"];
+        teddyDescription.innerHTML = response["description"];
+        teddyPrice.innerHTML = "Prix : " + response["price"] / 100 + "€";
+})
 
 // Choix de la couleur
+let colorStorage = 0;
 teddyGet()
     .then(function (response) {
         for (i = -1; i < response["colors"].length; i++) {
             if (i < 0) {
-                let x = teddy.appendChild(divDescription).appendChild(divColor).appendChild(choiceColor);
                 let option = document.createElement("option");
                 option.text = "---";
-                x.add(option);
+                choiceColor.add(option);
             }
             else {
-                let x = teddy.appendChild(divDescription).appendChild(divColor).appendChild(choiceColor);
                 let option = document.createElement("option");
                 option.text = response["colors"][i];
                 option.setAttribute("value", option.text);
-                x.add(option);
-                sessionStorage.removeItem("color");
+                choiceColor.add(option);
                 choiceColor.addEventListener("click", function (event) {
-                    if (x.value != "---") {
-                        let colorStorage = x.value;                        
+                    if (choiceColor.value != "---") {
                         event.preventDefault();
-                        sessionStorage.setItem("color", colorStorage);
+                        colorStorage = choiceColor.value;
                     }
                 })
-
-
             }
         }
-
     })
 
 // Choix de la quantité
+let amountStorage = 1;
 teddyGet()
     .then(function () {
-        let nombreTeddies = document.createElement("p");
-        nombreTeddies.innerHTML = "Quantité : ";
-        teddy.appendChild(divDescription).appendChild(divQuantité).appendChild(nombreTeddies);
-        let amount = document.createElement("select");
-        var xAmount = teddy.appendChild(divDescription).appendChild(divQuantité).appendChild(amount);
-        for (d = 1; d <= 3; d++) {
+        for (d = 1; d <= 5; d++) {
             var optionAmount = document.createElement("option");
             optionAmount.text = d
-            xAmount.add(optionAmount);
-            sessionStorage.setItem("amount", "1")
-            amount.addEventListener("click", function (event) {
-                let amountStorage = xAmount.value
+            choiceAmount.add(optionAmount);
+            choiceAmount.addEventListener("change", function (event) {
                 event.preventDefault();
-                sessionStorage.setItem("amount", amountStorage);
+                amountStorage = choiceAmount.value
             });
         }
     })
 
-// Ajout du bouton commande
+// Ajout au panier
 teddyGet()
     .then(function (response) {
-        addCart.type = "submit";
-        teddy.appendChild(divDescription).appendChild(addCart).innerHTML = "Ajouter au panier";
         addCart.addEventListener("click", function (event) {
-            if (sessionStorage.getItem("color")) {
+            if (colorStorage != 0) {
                 event.preventDefault();
-                sessionStorage.setItem("id", response["_id"]);
-                sessionStorage.setItem("name", response["name"]);
-                sessionStorage.setItem("price", response["price"] / 100);
-                sessionStorage.setItem("image", response["imageUrl"]);
+                let article = (JSON.stringify({
+                    idArticle : response["id"],
+                    nameArticle : response["name"],
+                    imageArticle : response["imageUrl"],
+                    priceArticle : response["price"] / 100,
+                    colorArticle : colorStorage,
+                    amountArticle : amountStorage,
+                }));
+                let nomArticle = response["id"];
+                localStorage.setItem("nomArticle", article);
                 window.location = "panier.html";
             }
             else {
